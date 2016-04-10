2e522b72a74af89446a6220db60a5e9d42ee190c
Yimeng Zhang <zym1010@gmail.com>
2016-04-09 20:29:24 -0400

    add timer

--- convert_vanhateren_debug.py
+++ convert_vanhateren_debug.py
@@ -4,6 +4,19 @@ import h5py
 import numpy as np
 import os.path
 import scipy.io as sio
+import time
+
+
+class Timer(object):
+    def __init__(self, name=None):
+        self.name = name
+
+    def __enter__(self):
+        self.tstart = time.time()
+
+    def __exit__(self, type, value, traceback):
+        print(self.name)
+        print('Elapsed: {}'.format(time.time() - self.tstart))
 
 
 def main():
@@ -12,23 +25,28 @@ def main():
     iml_dataset = file_handle['iml_data']
 
     for chunk_idx in range(42):
-        if chunk_idx < 41:
-            imc_subset_hdf5 = imc_dataset[:, :, chunk_idx * 100:(chunk_idx + 1) * 100]
-            iml_subset_hdf5 = iml_dataset[:, :, chunk_idx * 100:(chunk_idx + 1) * 100]
-        else:
-            imc_subset_hdf5 = imc_dataset[:, :, chunk_idx * 100:]
-            iml_subset_hdf5 = iml_dataset[:, :, chunk_idx * 100:]
-        iml_subset_mat = sio.loadmat(os.path.join('/home/leelab_share/datasets/vanHateren_iml_MATLAB',
-                                                  'vanHaterenIML_{:02d}.mat'.format(chunk_idx + 1)))['vanHaterenIML'][
-            0, 0]['images']
-        imc_subset_mat = sio.loadmat(os.path.join('/home/leelab_share/datasets/vanHateren_imc_MATLAB',
-                                                  'vanHaterenIMC_{:02d}.mat'.format(chunk_idx + 1)))['vanHaterenIMC'][
-            0, 0]['images']
+        with Timer('read hdf5'):
+            if chunk_idx < 41:
+                imc_subset_hdf5 = imc_dataset[:, :, chunk_idx * 100:(chunk_idx + 1) * 100]
+                iml_subset_hdf5 = iml_dataset[:, :, chunk_idx * 100:(chunk_idx + 1) * 100]
+            else:
+                imc_subset_hdf5 = imc_dataset[:, :, chunk_idx * 100:]
+                iml_subset_hdf5 = iml_dataset[:, :, chunk_idx * 100:]
+        with Timer('read mat'):
+            iml_subset_mat = sio.loadmat(os.path.join('/home/leelab_share/datasets/vanHateren_iml_MATLAB',
+                                                      'vanHaterenIML_{:02d}.mat'.format(chunk_idx + 1)))[
+                'vanHaterenIML'][
+                0, 0]['images']
+            imc_subset_mat = sio.loadmat(os.path.join('/home/leelab_share/datasets/vanHateren_imc_MATLAB',
+                                                      'vanHaterenIMC_{:02d}.mat'.format(chunk_idx + 1)))[
+                'vanHaterenIMC'][
+                0, 0]['images']
         assert np.array_equal(iml_subset_mat, iml_subset_hdf5)
         assert np.array_equal(imc_subset_mat, imc_subset_hdf5)
         print(chunk_idx)
 
     file_handle.close()
+
 
 if __name__ == '__main__':
     main()
